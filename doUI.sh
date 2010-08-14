30606b064aebf28f284be0016c0d6c9ede8824b9
Russ Ferriday <russf@topia.com>
2010-08-14 01:40:49 +0000

    fix failure on final plone install - missing extension

--- doUI.sh
+++ doUI.sh
@@ -6,7 +6,7 @@ echo 'getting the latest Universal Installer'
 curl -OL http://launchpad.net/plone/4.0/4.0rc1/+download/Plone-4.0rc1-UnifiedInstaller.tgz
 tar xzf Plone-4.0rc1-UnifiedInstaller.tgz
 cd Plone-4.0rc1-UnifiedInstaller
-./install zeo
+./install.sh zeo
 
 
 echo 'Plone setup complete'
