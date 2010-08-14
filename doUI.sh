7e8467d56874d75817ba6bea9094959bb6141709
Russ Ferriday <russf@topia.com>
2010-08-14 00:35:23 +0000

    added detailed setup items

---
+++ doUI.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#This is the most volatile part of the install
+
+echo 'getting the latest Universal Installer'
+curl -OL http://launchpad.net/plone/4.0/4.0rc1/+download/Plone-4.0rc1-UnifiedInstaller.tgz
+tar xzf Plone-4.0rc1-UnifiedInstaller.tgz
+cd Plone-4.0rc1-UnifiedInstaller
+./install zeo
+
+
+echo 'Plone setup complete'
+
+
