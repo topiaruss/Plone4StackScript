27a8ababe58ffefcc9979a1350f856b0f40c55f5
Russ Ferriday <russf@topia.com>
2011-02-11 01:39:38 -0800

    fixed locations

--- adjustPermissions.sh
+++ adjustPermissions.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 cd /root/Plone4StackScript
-cp buildout.cfg /home/plone/Plone/zeocluster/new.buildout.cfg
-cp extended-buildout.cfg  /home/plone/Plone/zeocluster
-cd /home/plone/Plone/zeocluster
+cp buildout.cfg /home/plone/Plone4/zeocluster/new.buildout.cfg
+cp extended-buildout.cfg  /home/plone/Plone4/zeocluster
+cd /home/plone/Plone4/zeocluster
 chown plone:plone *.cfg
 
